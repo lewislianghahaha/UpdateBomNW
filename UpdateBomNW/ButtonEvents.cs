a792639817f51716ae53d0d208e1876f83a08355
Lewis <heshanpin2016@163.com>
2023-12-09 14:05:19 +0800

    20231209添加限制条件

--- UpdateBomNW/ButtonEvents.cs
+++ UpdateBomNW/ButtonEvents.cs
@@ -17,32 +17,43 @@ namespace UpdateBomNW
 
             if (e.BarItemKey == "tbUpdateBomNw")
             {
-                //获取列表上通过复选框勾选的记录
-                var selectedrows = this.ListView.SelectedRowsInfo;
-                //判断若没有选择记录,即报出异常提示
-                if (selectedrows.Count == 0)
+                //获取当前登录用户
+                var username = this.Context.UserName;
+
+                //todo:只有指定人员才能执行
+                if (username != "刘笑梅")
                 {
-                    View.ShowErrMessage("没有选择记录,不能进行更新");
+                    View.ShowErrMessage($"用户:'{username}'没有权限,不能执行此操作");
                 }
                 else
                 {
-                    //通过循环将选中行的主键进行收集
-                    foreach (var row in selectedrows)
+                    //获取列表上通过复选框勾选的记录
+                    var selectedrows = this.ListView.SelectedRowsInfo;
+                    //判断若没有选择记录,即报出异常提示
+                    if (selectedrows.Count == 0)
                     {
-                        if (string.IsNullOrEmpty(fmaterialidlist))
-                        {
-                            fmaterialidlist = "'" + Convert.ToString(row.PrimaryKeyValue) + "'";
-                        }
-                        else
-                        {
-                            fmaterialidlist += "," + "'" + Convert.ToString(row.PrimaryKeyValue) + "'";
-                        }
+                        View.ShowErrMessage("没有选择记录,不能进行更新");
                     }
-                    //根据所获取的单号进行更新
-                    generate.UpdateBomNw(fmaterialidlist);
+                    else
+                    {
+                        //通过循环将选中行的主键进行收集
+                        foreach (var row in selectedrows)
+                        {
+                            if (string.IsNullOrEmpty(fmaterialidlist))
+                            {
+                                fmaterialidlist = "'" + Convert.ToString(row.PrimaryKeyValue) + "'";
+                            }
+                            else
+                            {
+                                fmaterialidlist += "," + "'" + Convert.ToString(row.PrimaryKeyValue) + "'";
+                            }
+                        }
+                        //根据所获取的单号进行更新
+                        generate.UpdateBomNw(fmaterialidlist);
 
-                    //输出
-                    View.ShowMessage("更新已完成");
+                        //输出
+                        View.ShowMessage("更新已完成");
+                    }
                 }
             }
 
